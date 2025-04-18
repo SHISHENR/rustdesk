8db46b66b89acb3546a03775ac421438d24a338a
SHISHEN <93456897+SHISHENR@users.noreply.github.com>
2025-04-18 15:19:33 +0800

    更新API

--- src/common.rs
+++ src/common.rs
@@ -944,7 +944,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://172.16.207.240:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
